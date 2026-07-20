ce8bdb48da6a0adae7572ff0f1d067f8e6b46006
Egwame Nicholas <nicosheg0123@gmail.com>
2026-07-20 22:36:21 +0100

    Update test-ocr-space.js

--- pages/api/test-ocr-space.js
+++ pages/api/test-ocr-space.js
@@ -1,10 +1,14 @@
 export default async function handler(req, res) {
   try {
+    const tinyWhiteImage = 'iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAACXBIWXMAAAsTAAALEwEAmpwYAAAA' +
+      'B3RJTUUH5gEBCwQCMQ0yNQAAAAd0RVh0QXV0aG9yAK4uz6cAAAAIdEVYdERlc2NyaXB0aW9u' +
+      'AAD/'.repeat(10) + 'AAAAElFTkSuQmCC';
+      
     const response = await fetch('https://api.ocr.space/parse/image', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        base64Image: 'data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==',
+        base64Image: `data:image/png;base64,${tinyWhiteImage}`,
         apikey: 'helloworld',
         language: 'eng',
         isOverlayRequired: false,
