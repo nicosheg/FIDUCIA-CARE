c2d015bc69f4c8dac0d75b009aa6b8b3af22859f
Egwame Nicholas <nicosheg0123@gmail.com>
2026-08-06 22:37:43 +0100

    Fix import path for getAttentionItems function

--- pages/api/care-queue.js
+++ pages/api/care-queue.js
@@ -1,4 +1,4 @@
-import { getAttentionItems } from '../../../lib/ariaIntelligence';
+import { getAttentionItems } from '../../lib/ariaIntelligence';
 
 export default async function handler(req, res) {
   const orgId = req.query.organization_id || 'demo-org';
@@ -8,4 +8,4 @@ export default async function handler(req, res) {
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-                          }
+}
