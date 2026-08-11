159a9956008bd103f30488efc16f3ba4d7e899e7
Egwame Nicholas <nicosheg0123@gmail.com>
2026-08-11 09:19:58 +0100

    Fix import path for database module in delete.js

--- pages/api/people/delete.js
+++ pages/api/people/delete.js
@@ -1,5 +1,5 @@
 // pages/api/people/delete.js
-import pool from '../../lib/db';
+import pool from '../../../lib/db';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
