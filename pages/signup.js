3612c0ada30caa88546a91fe73b09bf76715ac50
Egwame Nicholas <nicosheg0123@gmail.com>
2026-08-29 15:21:30 +0100

    Update Signup component useEffect dependency
    Add router as a dependency to useEffect for Signup component.

--- pages/signup.js
+++ pages/signup.js
@@ -1,11 +1,15 @@
 // pages/signup.js
+// Signup entry point. Authentication UI lives in pages/login.js.
+
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
-export default function Signup() {
-    const router = useRouter();
-    useEffect(() => {
-        router.replace('/login?mode=signup');
-    }, []);
-    return null;
+export default function Signup(){
+  const router=useRouter();
+
+  useEffect(()=>{
+    router.replace('/login?mode=signup');
+  },[router]);
+
+  return null;
 }
