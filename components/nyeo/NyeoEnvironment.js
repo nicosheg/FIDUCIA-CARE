c9cd2b4b5ba8d8076ad8758997286ecd60e98fd2
Egwame Nicholas <nicosheg0123@gmail.com>
2026-08-30 14:24:29 +0100

    Create NyeoEnvironment component with cloud and haze

---
+++ components/nyeo/NyeoEnvironment.js
@@ -0,0 +1,11 @@
+// components/nyeo/NyeoEnvironment.js
+export default function NyeoEnvironment(){
+  return <div className="nyeo-environment" aria-hidden="true">
+    <div className="nyeo-cloud c1"/>
+    <div className="nyeo-cloud c2"/>
+    <div className="nyeo-cloud c3"/>
+    <div className="nyeo-cloud c4"/>
+    <div className="nyeo-haze h1"/>
+    <div className="nyeo-haze h2"/>
+  </div>;
+}
